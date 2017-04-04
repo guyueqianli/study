ae35e6d0c19265e37954268faa55ade300028936
guyueqianli <770235965@qq.com>
2017-04-04 18:53:37 +0800

    Update linked_list.php

--- linked_list.php
+++ linked_list.php
@@ -1,15 +1,12 @@
 <?php
 //链表节点
 class Node {
-    //节点id
-    public $id;
-    //节点名称
-    public $name;
+    //val
+    public $val;
     //下一节点
     public $next;
-    public function __construct($id, $name) {
-        $this->id = $id;
-	$this->name = $name;
+    public function __construct($val) {
+        $this->val = $val;
 	$this->next = null;
     }
 }
@@ -17,58 +14,54 @@ class Node {
 //单链表
 class SingelLinkList {
     private $header; //链表头节点
-    public function __construct($id = null, $name = null) {
-        $this->header = new node ($id, $name, null);
+    public function __construct($val = null) {
+	$this->header = new node ($val, null);
     }
     //添加节点数据
     public function addLink($node) {
         $current = $this->header;
-	while ($current->next != null ) {
-	    if($current->next->id > $node->id) {
-	        break;
-	    }
-	    $current = $current->next;
-	}
-	$node->next = $current->next;
-	$current->next = $node;
-	//echo "--add : --\n";var_dump($current);
+        while ($current->next != null ) {
+            if($current->next->val > $node->val) {
+                break;
+            }
+            $current = $current->next;
+        }
+        $node->next = $current->next;
+        $current->next = $node;
+        //echo "--add : --\n";var_dump($current);
     }
-    
+
     //获取链表
     public function getLinkList() {
         $current = $this->header;
-	//echo "--getLinkList : --\n";var_dump($current);
-	$str = "链表为空!";
-	if($current->next == null) {
-	    return $str;
-	}
-	$arr = array();
-	while($current->next != null ) {
-	    $id = $current->next->id;
-	    $name = $current->next->name;
-	    // $current = $current->next->next;
-	    // echo "--current->next : -- \n";var_dump($current);
-	    $arr[] = array(
-	            'id' => $id, 
-		    'name' => $name,
-		    );
-	    if ($current->next->next == null) {
-	        break;
-	    }
-	    $current = $current->next;
-	}
-	$str = "\n";
-	foreach ($arr as $k => $v) {
-	    $str .= "id : ".$v['id'].", name : ".$v['name']."\n";
-	}
-	return $str;
+        //echo "--getLinkList : --\n";var_dump($current);
+        $str = "链表为空!";
+        if($current->next == null) {
+            return $str;
+        }
+        $arr = array();
+        while($current->next != null ) {
+            $val = $current->next->val;
+            // $current = $current->next->next;
+            // echo "--current->next : -- \n";var_dump($current);
+            $arr[] = array('val' => $val);
+                if ($current->next->next == null) {
+                    break;
+                }
+                $current = $current->next;
+        }
+        $str = "\n";
+        foreach ($arr as $k => $v) {
+            $str .= "val : ".$v['val']."\n";
+        }
+        return $str;
     }
 }
 
 $lists = new SingelLinkList();
-$lists->addLink(new Node(5, 'eeeeee'));
-$lists->addLink(new Node(1, 'aaaaaa'));
-$lists->addLink(new Node(6, 'ffffff'));
-$lists->addLink(new Node(2, 'bbbbbb'));
-$lists->addLink(new Node(3, 'cccccc'));
+$lists->addLink(new Node(5));
+$lists->addLink(new Node(1));
+$lists->addLink(new Node(6));
+$lists->addLink(new Node(2));
+$lists->addLink(new Node(3));
 echo $lists->getLinkList();
