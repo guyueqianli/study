46757c3968e04d7e0d70340a96b8b4934f03066a
guyueqianli <770235965@qq.com>
2017-04-07 01:38:56 +0800

    Create singelLinkListMiddle.php

---
+++ singelLinkListMiddle.php
@@ -0,0 +1,67 @@
+<?php
+//链表节点
+class Node {
+    //val
+    public $val;
+    //下一节点
+    public $next;
+    public function __construct($val) {
+        $this->val = $val;
+        $this->next = null;
+    }
+}
+//单链表
+class SingelLinkList {
+    private $header; //链表头节点
+    public function __construct($val = null) {
+        $this->header = new node ($val, null);
+    }
+    //添加节点数据
+    public function addLink($node) {
+        $current = $this->header;
+        $node->next = $current->next;
+        $current->next = $node;
+    }
+    //获取链表长度
+    public function getLinkLength() {
+        $current = $this->header;
+        $i = 0;
+        while ($current->next != null ) {
+            $i ++;
+            $current = $current->next;
+        }
+        return $i;
+    }
+    //获取中间节点（方法1：根据链表长度获取）
+    public function getMiddleNode() {
+        $current = $this->header;
+        $length = $this->getLinkLength();
+        if($length == 0) {
+            $str = "链表为空!";
+            return $str;
+        }
+        $middle = intval($length / 2);
+        if(1 == ($length % 2)) {
+            for($i = 0;$i <= $middle;$i++) {
+                $current = $current->next;
+            }
+            $str = "中间节点为：".$current->val;
+        }
+        if(0 == ($length % 2)) {
+            for($i = 0;$i < $middle;$i++) {
+                $current = $current->next;
+            }
+            $str = "中间节点为：".$current->val."和".$current->next->val;
+        }
+        return $str;
+    }
+}
+
+$lists = new SingelLinkList();
+$lists->addLink(new Node(5));
+$lists->addLink(new Node(3));
+$lists->addLink(new Node(1));
+$lists->addLink(new Node(4));
+$lists->addLink(new Node(7));
+$lists->addLink(new Node(8));
+echo $lists->getMiddleNode();
