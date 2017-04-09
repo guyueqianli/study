8c0257c2cd1d2b80cd7eea713435b710596c0200
guyueqianli <770235965@qq.com>
2017-04-09 16:53:41 +0800

    Update singelLinkListMiddle.php

--- singelLinkListMiddle.php
+++ singelLinkListMiddle.php
@@ -57,13 +57,105 @@ class SingelLinkList {
         }
         return $str;
     }
+    //取中间节点（方法2：根据快慢指针）
+    public function getMiddleNodeBySlowFast() {
+        $slow = $this->header;
+        $fast = $this->header;
+        $i = 0;
+        while($fast != null && $fast->next != null) {
+            $i++;
+            $slow = $slow->next;
+            //echo "\n --------- slow $i ---------------  \n";var_dump($slow);
+            $fast = $fast->next->next;
+            //echo "\n --------- fast $i ---------------  \n";var_dump($fast);
+        }
+        if(0 == $i) {
+            $str = "链表为空!";
+            return $str;
+        }
+        if($fast == null) {
+            $str = "中间节点为：".$slow->val;
+        }
+        if($fast->next == null && $fast != null) {
+            $str = "中间节点为：".$slow->val."和".$slow->next->val;
+        }
+        return $str;
+    }
 }
 
+echo "\n=========================== 1 ====================================\n";
 $lists = new SingelLinkList();
 $lists->addLink(new Node(5));
-$lists->addLink(new Node(3));
-$lists->addLink(new Node(1));
-$lists->addLink(new Node(4));
-$lists->addLink(new Node(7));
-$lists->addLink(new Node(8));
+//$lists->addLink(new Node(3));
+//$lists->addLink(new Node(1));
+//$lists->addLink(new Node(4));
+//$lists->addLink(new Node(7));
+//$lists->addLink(new Node(8));
+echo "\n方法一：\n";
 echo $lists->getMiddleNode();
+echo "\n方法二：\n";
+echo $lists->getMiddleNodeBySlowFast();
+
+echo "\n=========================== 2 ====================================\n";
+
+$lists2 = new SingelLinkList();
+$lists2->addLink(new Node(5));
+$lists2->addLink(new Node(3));
+echo "\n方法一：\n";
+echo $lists2->getMiddleNode();
+echo "\n方法二：\n";
+echo $lists2->getMiddleNodeBySlowFast();
+
+
+echo "\n=========================== 3  ====================================\n";
+
+$lists3 = new SingelLinkList();
+$lists3->addLink(new Node(5));
+$lists3->addLink(new Node(3));
+$lists3->addLink(new Node(1));
+echo "\n方法一：\n";
+echo $lists3->getMiddleNode();
+echo "\n方法二：\n";
+echo $lists3->getMiddleNodeBySlowFast();
+
+
+echo "\n==========================  4 =====================================\n";
+
+$lists4 = new SingelLinkList();
+$lists4->addLink(new Node(5));
+$lists4->addLink(new Node(3));
+$lists4->addLink(new Node(1));
+$lists4->addLink(new Node(4));
+echo "\n方法一：\n";
+echo $lists4->getMiddleNode();
+echo "\n方法二：\n";
+echo $lists4->getMiddleNodeBySlowFast();
+
+echo "\n=========================== 5 ====================================\n";
+
+$lists5 = new SingelLinkList();
+$lists5->addLink(new Node(5));
+$lists5->addLink(new Node(3));
+$lists5->addLink(new Node(1));
+$lists5->addLink(new Node(4));
+$lists5->addLink(new Node(7));
+echo "\n方法一：\n";
+echo $lists5->getMiddleNode();
+echo "\n方法二：\n";
+echo $lists5->getMiddleNodeBySlowFast();
+
+
+echo "\n=========================== 6 ====================================\n";
+
+$lists6 = new SingelLinkList();
+$lists6->addLink(new Node(5));
+$lists6->addLink(new Node(3));
+$lists6->addLink(new Node(1));
+$lists6->addLink(new Node(4));
+$lists6->addLink(new Node(7));
+$lists6->addLink(new Node(8));
+echo "\n方法一：\n";
+echo $lists6->getMiddleNode();
+echo "\n方法二：\n";
+echo $lists6->getMiddleNodeBySlowFast();
+
