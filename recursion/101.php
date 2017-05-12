5a67e9e58eadf6367ccaa4f1253f38eebd58f4f7
guyueqianli <770235965@qq.com>
2017-05-13 02:33:20 +0800

    递归101

--- recursion/101.php
+++ recursion/101.php
@@ -2,16 +2,13 @@
 /**
 *计算5的阶乘
 */
-
 function factorial($n) {
-	if ($n === 0) {
-		return 1;
-	}
-	if ($n > 0) {
-		return $n * factorial($n - 1);
-	}
+    if ($n === 0) {
+        return 1;
+    }
+    if ($n > 0) {
+        return $n * factorial($n - 1);
+    }
 }
-
-
 echo factorial(5);
 
