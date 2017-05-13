8bbfb1584c98fb6c5124a9a8514ca2e855597dfe
guyueqianli <770235965@qq.com>
2017-05-14 02:48:31 +0800

    linked list 101

---
+++ linked_list/101.php
@@ -0,0 +1,56 @@
+<?php
+/**
+* 使用PHP实现单链表
+*/
+
+//链表节点
+class Node {
+    //val
+    public $val;
+    //下一节点
+    public $next;
+    public function __construct($val) {
+        $this->val = $val;
+        $this->next = null;
+    }
+}
+//单链表
+class SingelLinkList {
+    private $header; //链表头节点
+    public function __construct($val = null) {
+        $this->header = new node ($val, null);
+    }
+    //添加节点数据
+    public function addLink($node) {
+        $current = $this->header;
+        while($current->next != null) {
+            $current = $current->next;
+        }
+        $current->next = $node;
+    }
+    //获取链表 val值
+    public function getLinkList($current) {
+        echo "\nval = ".$current->val;
+        if ($current->next != null) {
+            return $this->getLinkList($current->next);  
+        }
+    }
+    //获取链表
+    public function getLinkListVal() {
+        $current = $this->header;
+        $str = "链表为空!";
+        if($current->next == null) {
+            return $str;
+        }
+        $current = $current->next;
+        return $this->getLinkList($current);
+    }
+}
+
+$lists = new SingelLinkList();
+$lists->addLink(new Node(5));
+$lists->addLink(new Node(1));
+$lists->addLink(new Node(6));
+$lists->addLink(new Node(2));
+$lists->addLink(new Node(3));
+$lists->getLinkListVal();
